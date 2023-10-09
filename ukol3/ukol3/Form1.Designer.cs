ac1fb8ea17cb903424ffef5d5255555dd61a4e2b
MartinSyrovy <97514843+syrovym05@users.noreply.github.com>
2023-10-09 13:11:56 +0200

    Add files via upload

---
+++ ukol3/ukol3/Form1.Designer.cs
@@ -0,0 +1,71 @@
+﻿namespace ukol3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Vyžaduje se proměnná návrháře.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Uvolněte všechny používané prostředky.
+        /// </summary>
+        /// <param name="disposing">hodnota true, když by se měl spravovaný prostředek odstranit; jinak false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kód generovaný Návrhářem Windows Form
+
+        /// <summary>
+        /// Metoda vyžadovaná pro podporu Návrháře - neupravovat
+        /// obsah této metody v editoru kódu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(302, 56);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(155, 225);
+            this.listBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(343, 287);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Přidej";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(794, 386);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ ukol3/ukol3/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ukol3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+       
+        Form2 form2;
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (listBox1.Items.Count <= 10)
+            {
+                form2 = new Form2();
+
+                form2.Size = new Size(300, 200);
+
+
+                Button uloz = new Button();
+                uloz.DialogResult = DialogResult.OK;
+                uloz.Location = new Point(40, 89);
+                uloz.Size = new Size(75, 23);
+                uloz.Text = "Ulož";
+      
+
+                Button zrus = new Button();
+                zrus.DialogResult = DialogResult.Cancel;
+                zrus.Location = new Point(40, 115);
+                zrus.Size = new Size(75, 23);
+                zrus.Text = "Zrus";
+  
+
+                TextBox textBox = new TextBox();
+                textBox.Location = new Point(40, 40);
+                textBox.Size = new Size(188, 22);
+
+
+
+                form2.Controls.Add(textBox);
+                form2.Controls.Add(uloz);
+                form2.Controls.Add(zrus);
+
+                form2.ActiveControl = textBox;
+                form2.AcceptButton = uloz;
+           
+
+                if (form2.ShowDialog() == DialogResult.OK)
+                {                    
+                    if (textBox.Text != "") listBox1.Items.Add(textBox.Text);                  
+                }
+
+            }
+            else MessageBox.Show("V listboxu je již zapsáno 10 slov");
+        }
+    }
+}
